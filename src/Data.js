2906ea6882cf41207945d6955a2cf6752bc76aea
rawats123 <117648565+rawats123@users.noreply.github.com>
2024-12-24 17:05:56 +0530

    Inital commit

---
+++ src/Data.js
@@ -0,0 +1,68 @@
+// src/data.js
+
+export const projected = [
+    {
+      title: "Cost Sheet System",
+      subtitle: "PHP, Codeigniter4 and mySql",
+      description:
+        "This project is based on COST SHEET SYSTEM, where there are 3 major role, Super ADMIn and admin then user, user creates the sheet of Raw material, then go for approval, admin will manage the all things and all are depend into the masters of sheets",
+      image: "/uploads/php.jpg",
+      link: "",
+    },
+
+    {
+        title: "My Portofolio",
+        subtitle: "ReactJs",
+        description:
+          "Create my portofolio using React js, this is first project in ReactJs where i build my portofolio",
+        image: "/uploads/js.jpg",
+        link: "",
+      }
+  ];
+
+  //skills
+  export const skills = [
+    {
+        title: "Html, CSS, PHP, Codeigniter, Mysql",
+        subtitle: "Core, Framework",
+        description:"Having 4 years of experience in core PHP and its framewok codeigniter",
+        image: "/uploads/php.jpg",
+        link : "",
+
+    },
+    {
+        title: "JavaScript,JSON, jQuery, AJAX",
+        subtitle: "Core, Framework",
+        description:"",
+        image: "/uploads/php.jpg",
+        link : "",
+
+    },
+    {
+        title: "ReactJS, NodeJs, MongoDB",
+        subtitle: "Core, Framework",
+        description:"Beginner",
+        image: "/uploads/php.jpg",
+        link : "",
+
+    },
+    {
+        title: "GIT, GitHub",
+        subtitle: "Core, Framework",
+        description:"Beginner",
+        image: "/uploads/php.jpg",
+        link : "",
+
+    },
+
+  ];
+
+  //testimonial
+  export const testimonials = [
+    {
+        name:"Ankur Gupta",
+        company:"Own",
+        image:"/uploads/user.webp",
+        quote:""
+    }
+  ];
